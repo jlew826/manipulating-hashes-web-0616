fa627c7185524b625c00458e9a2a8e309f02c8b2
jlew826 <jlew826@gmail.com>
2016-06-08 13:08:20 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,19 @@ def first_challenge
   }
 
   #your code here
+contacts.each do |name, data|
+  #"Jon Snow" or "Freddy Mercury"
+  data.each do |attribute, value|
+    #attribute = :name, value = "Jon"
+    if attribute == :favorite_icecream_flavors
+      value.delete_if {|x| x == "strawberry"}
+      
 
+    end
+  end
+end
+
+      
 
   #remember to return your newly altered contacts hash!
   contacts
